195a15b29cfe5fdabb327a490c4c2ad48b99c853
EaylZwigenbom <flemenko5@gmail.com>
2026-04-10 16:05:59 +0300

    cool
    ### Latest Changes
    1. Added changelog.md
    2. added cli arguments -> pcap file, output vsdx file. no extension verification yet.

---
+++ changelog.md
@@ -0,0 +1,3 @@
+### Latest Changes
+1. Added changelog.md
+2. added cli arguments -> pcap file, output vsdx file. no extension verification yet.
--- src/netmapper/cli.py
+++ src/netmapper/cli.py
@@ -1,8 +1,14 @@
 import argparse
 import scapy
 
-def main():
+def main(pcap_file, output_file):
     ...
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Welcome to netmapper - turn pcap files into visual network diagrams")
+    
+    parser.add_argument("pcap-file", help="The pcap file that sniffed ")
+    parser.add_argument("-o", "--output")
+
+    args = parser.parse_args()
     main()
